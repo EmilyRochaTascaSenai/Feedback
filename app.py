7480e576143bf25f5a2ca96b9bddc2e7ead10ff8
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-02-25 11:29:25 -0300

    fazendo sql no python

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask  import Flask,render_template
+from flask  import Flask,render_template,form,request,redirect
+import datetime
+import _mysql_connector
 app = Flask(__name__)
 
 #CRIANDO A ROTA incial
@@ -7,8 +9,46 @@ app = Flask(__name__)
 def pagina_incial():
     return  render_template("index.html")
 
-app.run(debug=True)
+
 
 # Criando rota cadastro
-@app.route("/post/cadastro")
+@app.route("/post/cadastro",methods=["POST"])
 def pagina_cadstro():
+    #Peguei as informações vinda do formulario
+   usuario = request.form.get("usuario")
+   mensagem= request.form.get("mensagem")
+   data_hora=datetime.datetime.today()
+    # Cadastrando as informações no banco de dados
+    # Criando a conexão
+   
+#    conexao = mysql.connector.connect(hostname="localhost",
+#                                         port=3306,
+#                                          user="root"
+#                                          password="root",
+#                                          database=dbFeedback)
+#    O cursor será responsavel por manipular o bcd
+   
+    # cursor =conexao.cursor()
+# Criando o sql que será executado
+   
+#    sql="""INSERT INTO tbComentarios
+#    (Nome,DataPostagem,Comentario)
+#    VALUES
+#    (%s, %s, %s)"""  
+   valores = (usuario,data_hora,mensagem)
+
+#    Executando o comenado sql
+#    cursos.execute(sql,valores)
+   
+# Confirmo a alteração 
+
+#    conexao.commit()
+   
+# Fecho a conexão com o banco
+   
+    # cursor.close()
+#    conexao.close()
+   
+# Redireciona para o index
+   return redirect("/")
+app.run(debug=True)
