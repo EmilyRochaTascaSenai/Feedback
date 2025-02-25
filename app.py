153f13fade624f7f1cd8030c9794732f774dc5c8
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-02-25 10:07:46 -0300

    criando rota cadastro e css e html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask  import Flask
+from flask  import Flask,render_template
 app = Flask(__name__)
 
-#CRIANDO A ROTA teste
+#CRIANDO A ROTA incial
 
 @app.route("/")
-def pagina_teste():
-    return "Pagina Principal" 
+def pagina_incial():
+    return  render_template("index.html")
 
 app.run(debug=True)
+
+# Criando rota cadastro
+@app.route("/post/cadastro")
+def pagina_cadstro():
